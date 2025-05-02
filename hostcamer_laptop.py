ab8c1de9f3c2eadf9143f961aa24a36cc105ce92
JohanJohanJohan <55000709+DepressedCaitlynMain@users.noreply.github.com>
2025-05-03 00:44:27 +0200

    Add files via upload

---
+++ hostcamer_laptop.py
@@ -0,0 +1,27 @@
+import cv2
+from flask import Flask, Response
+import threading
+
+app = Flask(__name__)
+camera = cv2.VideoCapture(0)
+
+def generate_frames():
+    while True:
+        success, frame = camera.read()
+        if not success:
+            continue
+
+        ret, buffer = cv2.imencode('.jpg', frame)
+        if not ret:
+            continue
+
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, threaded=True)
